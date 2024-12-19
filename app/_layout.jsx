c5555f68e0509ed2d65f9eb29a32c9692e44944a
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-12-19 22:50:06 +0000

    refactor: update TypeScript configuration and remove unused layout files

---
+++ app/_layout.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import React from 'react'
+
+export default function _layout  () {
+  return (
+    <View style={styles.container}>
+      <Text>_layout</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
