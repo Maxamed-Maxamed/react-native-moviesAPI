0fcc57c14d076fb33fe5cc9abaa3cbe129c06d68
Maxamed-Maxamed <20088349@mail.wit.ie>
2025-01-08 13:57:51 +0000

    feat: enhance tab layout with custom icons and styling for Home screen

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
+import { icons } from '../../constants'
+import '../../global.css'
+
+const TabIcon = ({ color, name, focused, icon }) => {
+  return (
+    <View className="items-center justify-center mt-2 space-y-1">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text className={`text-xs ${focused ? 'font-semibold text-black' : 'text-gray-500'}`}>
+        {name}
+      </Text>
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 65,
+          paddingBottom: 8,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              name="Home"
+              focused={focused}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   )
 }
 
